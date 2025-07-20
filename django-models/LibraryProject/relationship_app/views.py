c413bb6d78c3c79364c4f50d4f485c3e6579cc4c
beabzk <92110044+beabzk@users.noreply.github.com>
2025-07-21 00:03:19 +0300

    Refactor views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,7 +15,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-# Add this import for the permission decorator
+
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 
 from .models import Book, Library, Author # Add Author here for the form
